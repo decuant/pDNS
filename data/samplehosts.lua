c87fd3269398ed3679a3cbfd0fe777dfdcfe9958
decuant <7431711+decuant@users.noreply.github.com>
2021-07-22 15:20:08 +0200

    Add files via upload

---
+++ data/samplehosts.lua
@@ -0,0 +1,51 @@
+--[[
+*	samplehosts.lua
+*
+*	Some sample hosts for testing.
+*
+]]
+
+-- ----------------------------------------------------------------------------
+-- list of test urls
+--
+local m_tHostURLs =
+{ 	
+	"worldweather.wmo.int",
+	"www.ildubbio.news",
+	"www.barbarianfc.co.uk",
+	"support.microsoft.com",
+	"www.example.com",
+	"lua-users.org",
+	"pcsupport.lenovo.com",
+	"news.ycombinator.com",
+	"graphics.stanford.edu",
+	"www.ilfattoquotidiano.it",
+	"root-servers.org",
+	"time.com",
+	"www.wordreference.com",
+	"photo.net",
+	"www.opensubtitles.org",
+	"worldarchery.sport",
+	"en.wikipedia.org",
+	"dillinger.io",
+	"wxwidgets.org",
+	"docs.microsoft.com",
+	"satellites.pro",
+	"english.alarabiya.net",
+	"www.facebook.com",
+	"www.baobabs.com",
+	"www.gmail.com",
+	"tineye.com",
+	"www.nasa.gov",
+	"www.imdb.com",
+	"it.glosbe.com",
+	"olympics.com",
+	"www.thisanerror.xyz",
+	"www.photo.net",
+	"it.yahoo.com",
+}
+
+return m_tHostURLs
+
+-- ----------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------
